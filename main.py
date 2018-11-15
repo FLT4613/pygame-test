573644603ac433e8c26c49d01e696954fc7ba874
FLT <ft4613st@gmail.com>
2018-11-15 20:49:51 +0900

    ボールを動かしてみる

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ pygame.init()
 screen = pygame.display.set_mode((640, 480))
 screen.fill((255, 255, 255))
 
+player_image = pygame.image.load("ball.gif")
+player = player_image.get_rect()
+x, y = 0, 0
 while 1:
+    player.center = (x, y)
+
+    screen.blit(player_image, player)
+
+    x += 1
+    y += 1
+    pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -13,4 +23,3 @@ while 1:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
-    pygame.display.flip()
