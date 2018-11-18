ba68b5a086009a1c1b9cc68a6b68ca8eeecda5de
FLT <ft4613st@gmail.com>
2018-11-18 13:26:00 +0900

    プレイヤーのSprite化

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 import sys
 import pygame
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("ball.gif")
+        self.rect = self.image.get_rect()
+        self.vx = 0
+        self.vy = 0
+        self.brake = 0.2
+        self.speed = 5.0
+
+    def update(self):
+        self.rect.move_ip(self.vx, self.vy)
+        if self.vx > 0.0:
+            self.vx = max(self.vx-self.brake, 0)
+        if self.vx < 0.0:
+            self.vx = min(self.vx+self.brake, 0)
+        if self.vy > 0.0:
+            self.vy = max(self.vy-self.brake, 0)
+        if self.vy < 0.0:
+            self.vy = min(self.vy+self.brake, 0)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
+player = Player()
 screen.fill((255, 255, 255))
 
-player_image = pygame.image.load("ball.gif")
-player = player_image.get_rect()
-x, y = 0, 0
 while 1:
-    player.center = (x, y)
-
     screen.fill((0, 0, 255))
-    screen.blit(player_image, player)
+    pressed_keys = pygame.key.get_pressed()
 
-    x += 1
-    y += 1
+    if pressed_keys[pygame.K_a]:
+        player.vx = -player.speed
+    elif pressed_keys[pygame.K_d]:
+        player.vx = player.speed
+    elif pressed_keys[pygame.K_w]:
+        player.vy = -player.speed
+    elif pressed_keys[pygame.K_s]:
+        player.vy = player.speed
+
+    player.update()
+    player.draw(screen)
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
