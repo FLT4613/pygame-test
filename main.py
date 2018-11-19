6dde8bf15b985af72fb6034437e54d6c10a5cf80
FLT <ft4613st@gmail.com>
2018-11-19 21:46:19 +0900

    球を飛ばす

--- main.py
+++ main.py
@@ -23,13 +23,31 @@ class Player(pygame.sprite.Sprite):
         if self.vy < 0.0:
             self.vy = min(self.vy+self.brake, 0)
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, start, velocity, speed=5.0):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = pygame.image.load("ball.gif")
+        self.rect = self.image.get_rect()
+        self.rect.center = start
+        self.vx = velocity[0]
+        self.vy = velocity[1]
+        self.speed = 5.0
+
+    def update(self):
+        self.rect.move_ip(self.vx, self.vy)
+        if not pygame.Rect(0, 0, 640, 480).contains(self.rect):
+            self.kill()
 
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 player = Player()
+
+group = pygame.sprite.RenderUpdates()
+group.add(player)
+
+Bullet.containers = group
 
 while 1:
     screen.fill((255, 255, 255))
@@ -43,10 +61,13 @@ while 1:
         player.vy = -player.speed
     if pressed_keys[pygame.K_s]:
         player.vy = player.speed
+    if pressed_keys[pygame.K_z]:
+        Bullet(player.rect.center, (10, 0))
 
-    player.update()
-    player.draw(screen)
+    group.update()
+    group.draw(screen)
     pygame.display.update()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
